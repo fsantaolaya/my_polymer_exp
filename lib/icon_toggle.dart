d3938188561e444cfaa2402e05fd58a9d00995c7
xe52360 <fernando.santa.contractor@bbva.com>
2016-09-21 19:19:14 +0200

    Create a icon toggle element taht fires a 'ready' event

---
+++ lib/icon_toggle.dart
@@ -0,0 +1,18 @@
+@HtmlImport('icon_toggle.html')
+library pdcl_first_elements.lib.icon_toggle;
+
+import 'dart:html';
+import 'package:polymer_elements/iron_icons.dart';
+import 'package:polymer_elements/iron_icon.dart';
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart' show HtmlImport;
+
+@PolymerRegister('icon-toggle')
+class IconToggle extends PolymerElement {
+
+  IconToggle.created() : super.created();
+
+  void ready() {
+    print("$runtimeType::ready()");
+  }
+}
