230f9436ee8772ec764c56870a5ff2adc20c1a48
xe52360 <fernando.santa.contractor@bbva.com>
2016-09-21 16:06:11 +0200

    Initial state just created

---
+++ README.md
@@ -0,0 +1,3 @@
+# my_polymer_exp
+
+A web app built using polymer.dart. from https://dart.academy/polymer-dart-code-lab-your-first-elements/
---
+++ web/index.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2016, xe52360. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:my_polymer_exp/main_app.dart';
+import 'package:polymer/polymer.dart';
+
+/// [MainApp] used!
+main() async {
+  await initPolymer();
+}
